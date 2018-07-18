cb7021ca77f3e59304967911feb87c5ca296f7a1
stoner6593 <stoner6593@gmail.com>
2018-07-18 11:50:11 -0500

    Cambios en alquiler

--- admin/include/alquiler/prg_alquiler-finalizar.php
+++ admin/include/alquiler/prg_alquiler-finalizar.php
@@ -9,7 +9,7 @@ $xidalquiler = $_GET['idalquiler'];
 $xidhabitacion = $_GET['idhabitacion'];
 
 $consulta="update habitacion set
-	idestado = 4,
+	idestado = 1,
 	idalquiler = 0
 	where idhabitacion = '$xidhabitacion'";
 
@@ -22,6 +22,22 @@ if($mysqli->query($consulta)){
 }else{
 		$Men = "Ha fallado, los datos no han sido registrados.";
 }
+
+
+/**
+ * Se agrego para el restaurant
+ */
+
+$consulta2="update habitacion set
+	idestado = 1
+	where idhabitacion = '$xidhabitacion'";
+
+if($mysqli->query($consulta2)){
+		$Men = "Los datos fueron guardados satisfactoriamente.";
+}else{
+		$Men = "Ha fallado, los datos no han sido registrados.";
+}
+
 $mysqli->close();	
 //$_SESSION['msgerror'] = $Men;
 header("Location: ../../control-habitaciones.php"); exit;
